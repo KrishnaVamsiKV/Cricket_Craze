6a43d78861ab194c59ff74d7de1f147120a13f8a
KrishnaVamsiKV <krishnavamsichkv@gmail.com>
2014-10-25 10:48:18 +0530

    Adding Consistency for batsmen and bowlers

--- consistency.R
+++ consistency.R
@@ -10,9 +10,9 @@ consistency = function(Batsman){
   last = len;
   Strike.Rate = c();
   Average = c();
-  for(i in 1:floor(len/10)){
-    match = matches[last-9:last];
-    last = last - 10;
+  for(i in 1:(last-9)){
+    match = matches[(last-9):last];
+    last = last - 1;
     runs = 0; balls = 0;outs = 0;
     for(j in match){
       batsmandata = Cricket[((Cricket$Batsman==Batsman)&(Cricket$Match.No==j)),];
@@ -37,4 +37,56 @@ consistency = function(Batsman){
   Batsman_rec$Average.Sd = round(sd(Average),2);
   Batsman_rec$Strike.Rate.Sd = round(sd(Strike.Rate),2);
   return(Batsman_rec);
+}
+
+bowler_consistency = function(bowler){
+  bowlerdata = Cricket[(Cricket$Bowler== bowler),];
+  matches = unique(bowlerdata$Match.No);
+  Bowler = data.frame(matrix(nrow=1,ncol=8));
+  names(Bowler) = c("Bowler","Country","Economy.Mean","Average.Mean","Dot.Percent.Mean"
+                         ,"Economy.Sd","Average.Sd","Dot.Percent.Sd");
+  Bowler$Bowler = bowler;
+  record = Cricket[(Cricket$Bowler==bowler),];
+  if(record[1,]$Batting.Team==record[1,]$Team1){
+    Bowler$Country = record[1,]$Team2;
+  }
+  else{
+    Bowler$Country = record[1,]$Team1;
+  }
+  len = length(matches);
+  last = len;
+  Economy = c();
+  Average = c();
+  Dot.Percent = c();
+  for(i in 1:(last-9)){
+    match = matches[(last-9):last];
+    last = last - 1;
+    runs = 0; balls = 0;outs = 0;dots = 0;
+    for(j in match){
+      bowlerdata = Cricket[((Cricket$Bowler== bowler)&(Cricket$Match.No==j)),];
+      runs = runs + sum(bowlerdata$Runs.Batsman)+sum(bowlerdata$Runs.Extras.No.Ball)+sum(bowlerdata$Runs.Extras.Wides);
+      balls = balls + length(bowlerdata$Ball.No[((bowlerdata$Runs.Extras.Wides==0)&(bowlerdata$Runs.Extras.No.Ball==0))]);
+      outs = outs + length(bowlerdata$Ball.No[(bowlerdata$Dismissed.Player!="")]);
+      dots = dots + length(bowlerdata$Ball.No[(bowlerdata$Total.Runs==0)]);
+    }
+    if(runs==0){
+      avg = 0;
+    }
+    else{
+      avg = round(runs/outs,2);
+      if(is.infinite(avg)){
+        avg = runs;
+      }
+    }
+    Economy = c(Economy,round(runs*6/balls,2));
+    Average = c(Average,avg);
+    Dot.Percent = c(Dot.Percent,round(dots*100/balls,2));
+  }
+  Bowler$Average.Mean = round(mean(Average),2);
+  Bowler$Economy.Mean = round(mean(Economy),2);
+  Bowler$Average.Sd = round(sd(Average),2);
+  Bowler$Economy.Sd = round(sd(Economy),2);
+  Bowler$Dot.Percent.Mean = round(mean(Dot.Percent),2);
+  Bowler$Dot.Percent.Sd = round(sd(Dot.Percent),2);
+  return(Bowler);
 }
--- data.R
+++ data.R
@@ -35,10 +35,44 @@ Team_records = as.data.frame(t(sapply(Teams,team_info)),row.names=FALSE);
 Team_performance = as.data.frame(t(sapply(Teams,team_performance)),row.names=FALSE);
 
 # Generating Consistence Criteria #
-summary(as.numeric(Batsmen_records$Matches))
-nrow(Batsmen_records[(Batsmen_records$Matches>15),])
-# This number is decent enough, so lets keep it a threshold #
-Batsmen_records_req = Batsmen_records[(Batsmen_records$Matches>15),];
-summary(as.numeric(Batsmen_records_req$Matches))
+Batsmen_records_req = Batsmen_records[(Batsmen_records$Runs>600),];
 Batsmen_req = as.character(Batsmen_records_req$Batsman);
 Batsmen_Consistency = as.data.frame(t(sapply(Batsmen_req,consistency)),row.names=FALSE);
+matches = c(1:74);
+Batsmen_req = as.character(Batsmen_Consistency$Batsman);
+for(i in 1:74){
+matches[i] = Batsmen_records$Matches[(Batsmen_records$Batsman==Batsmen_req[i])];
+}
+streaks = as.numeric(matches) - 10;
+Batsmen_Consistency$Streaks = streaks;
+rating = (1+4*(as.numeric(Batsmen_Consistency$Average.Mean)>=42.41)
+          +3*((as.numeric(Batsmen_Consistency$Average.Mean)<42.41)&(as.numeric(Batsmen_Consistency$Average.Mean)>=37.15))
+          +2*((as.numeric(Batsmen_Consistency$Average.Mean)<37.15)&(as.numeric(Batsmen_Consistency$Average.Mean)>=35.67))
+          +1*((as.numeric(Batsmen_Consistency$Average.Mean)<35.67)&(as.numeric(Batsmen_Consistency$Average.Mean)>=30.80)))
+Batsmen_Consistency$Rating = round((as.numeric(Batsmen_Consistency$Average.Mean)^2*(rating*as.numeric(Batsmen_Consistency$Strike.Rate.Mean))*as.numeric(Batsmen_Consistency$streaks))
+                                     /(as.numeric(Batsmen_Consistency$Average.Sd)),2);
+ 
+Batsmen_Consistency = Batsmen_Consistency[order(Batsmen_Consistency$Rating,decreasing=TRUE),];
+
+
+Bowlers_records_req = Bowlers_records[(Bowlers_records$Wickets>25),];
+Bowlers_req = as.character(Bowlers_records_req$Bowler);
+Bowlers_Consistency = as.data.frame(t(sapply(Bowlers_req,bowler_consistency)),row.names=FALSE);
+matches = c(1:59);
+Bowlers_req = as.character(Bowlers_Consistency$Bowler);
+for(i in 1:59){
+  matches[i] = Bowlers_records$Matches[(Bowlers_records$Bowler==Bowlers_req[i])];
+}
+streaks = as.numeric(matches) - 10;
+Bowlers_Consistency$Streaks = streaks;
+Bowlers_Consistency$WpM = round((as.numeric(Bowlers_Consistency$Economy.Mean))*10/(as.numeric(Bowlers_Consistency$Average.Mean)),2);
+rating = (1+4*(as.numeric(Bowlers_Consistency$Average.Mean)<=24.87)
+          +3*((as.numeric(Bowlers_Consistency$Average.Mean)<28.40)&(as.numeric(Bowlers_Consistency$Average.Mean)>=24.87))
+          +2*((as.numeric(Bowlers_Consistency$Average.Mean)<29.36)&(as.numeric(Bowlers_Consistency$Average.Mean)>=28.40))
+          +1*((as.numeric(Bowlers_Consistency$Average.Mean)<33.93)&(as.numeric(Bowlers_Consistency$Average.Mean)>=29.36)))
+
+Bowlers_Consistency$Rating = round(((as.numeric(Bowlers_Consistency$Dot.Percent.Mean))*(rating)*(as.numeric(Bowlers_Consistency$Streaks)))/
+                                     ((as.numeric(Bowlers_Consistency$Average.Mean))*(as.numeric(Bowlers_Consistency$Economy.Mean))*(as.numeric(Bowlers_Consistency$Average.Sd))),2);
+Bowlers_Consistency$WpM = round((as.numeric(Bowler_Consistency$Economy.Mean))*10/(as.numeric(Bowlers_Consistency$Average.Mean)),2);
+Bowlers_Consistency = Bowlers_Consistency[order(Bowlers_Consistency$Rating,decreasing=TRUE),];
+
